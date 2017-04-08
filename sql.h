f61e53d1687cb94e6476f9c81efd7315f8f4dc5c
WnBat <2606154438@qq.com>
2017-04-08 18:54:52 +0800

    Delete sql.h

--- sql.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SQL_H__
-#define __SQL_H__
-
-#include <sqlite3.h>
-
-extern sqlite3 *sqlite3_init(const char *path);
-extern void sqlite3_add_user(sqlite3 *db, const unsigned char *name, unsigned short id, const unsigned char spec[]);
-
-#endif
